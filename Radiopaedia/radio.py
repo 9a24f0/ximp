94caaba2bd20cd63b0c585e28cf9af70b4622c0a
Giang Nguyen <truonggiang2704@gmail.com>
2020-11-29 00:30:08 +0700

    scrape all search pages of Radio sth

--- Radiopaedia/radio.py
+++ Radiopaedia/radio.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = 'https://radiopaedia.org/search?lang=us&q=pneumonia&scope=cases&fbclid=IwAR1w46Ai6R5Yqiwucw6L_gM9o3dwVN_YC5pazixzs0oYf94r-lzODd1nKEI'
+URLS = 'https://radiopaedia.org/search?fbclid=IwAR1w46Ai6R5Yqiwucw6L_gM9o3dwVN_YC5pazixzs0oYf94r-lzODd1nKEI&lang=us&page={}&q=pneumonia&scope=cases'
 
 def getImgUrls(html):
 	urls = []
@@ -30,18 +30,21 @@ def getLabels(html):
 
 if __name__ == '__main__':
 	content = []
-	page = requests.get(URL)
-	soup = BeautifulSoup(page.content, 'html.parser')
-	result = soup.find(id='content')
-	urls = getImgUrls(result)
-	labels = getLabels(result)
-	
-	for i in range(len(urls)):
-		img = {
-		'title': labels[i],
-		'link': urls[i]
-		}
-		content.append(img)
+	for i in range(27):
+		page_number = str(i+1)
+		URL = URLS.format(page_number)
+		page = requests.get(URL)
+		soup = BeautifulSoup(page.content, 'html.parser')
+		result = soup.find(id='content')
+		urls = getImgUrls(result)
+		labels = getLabels(result)
+		
+		for i in range(len(urls)):
+			img = {
+			'title': labels[i],
+			'link': urls[i]
+			}
+			content.append(img)
 	
 	with open('radio_urls.json', 'w') as js:
 		json.dump(content, js, indent =4)
